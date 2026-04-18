444f6ecf132ebf2123b29ec1f9463c9ed4e27b07
Threepwood-7 <mt0z1@pir0z.yxz>
2026-04-18 22:43:18 +0200

    REF-026 move Common Controls dependency into manifests

--- srchybrid/Emule.cpp
+++ srchybrid/Emule.cpp
@@ -408,16 +408,6 @@ private:
 }
 
 
-#ifdef _UNICODE
-#if defined _M_X64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_ARM64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='arm64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#else
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#endif
-#endif
-
 CLogFile theLog;
 CLogFile theVerboseLog;
 bool g_bLowColorDesktop = false;
