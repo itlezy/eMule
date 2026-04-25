7cf1a7b6e2c12400e6ff45dfb8dc2e179f1b2ae7
MTU0Z <mtu@0zz.com>
2026-04-25 04:52:50 +0200

    BUG-063 require shared-dir delete confirmation

--- srchybrid/SharedDirsTreeCtrl.cpp
+++ srchybrid/SharedDirsTreeCtrl.cpp
@@ -987,7 +987,7 @@ BOOL CSharedDirsTreeCtrl::OnCommand(WPARAM wParam, LPARAM)
 		case MP_REMOVE:
 		case MPG_DELETE:
 			{
-				if (IDNO == LocMessageBox(IDS_CONFIRM_FILEDELETE, MB_ICONWARNING | MB_DEFBUTTON2 | MB_YESNO, 0))
+				if (LocMessageBox(IDS_CONFIRM_FILEDELETE, MB_ICONWARNING | MB_DEFBUTTON2 | MB_YESNO, 0) != IDYES)
 					return TRUE;
 
 				m_pSharedFilesCtrl->SetRedraw(false);
